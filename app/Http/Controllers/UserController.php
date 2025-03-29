5c78913c3f0a8727c7af8dec56ca45b34466ba21
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-03-29 13:27:06 -0300

    feat: creating index.html

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,11 +4,7 @@ namespace App\Http\Controllers;
 
 class UserController extends Controller
 {
-    public function getUsers() {
-        $users = [
-            ['id' => 1, 'name' => 'Daniel', 'email' => 'daniel@example.com'],
-            ['id' => 2, 'name' => 'Alice', 'email' => 'alice@example.com']
-        ];
-        return response()->json($users);
+    public function index() {
+        return view('index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 
-Route::get('/',[UserController::class, "getUsers"])->name("user.getUsers");
+Route::get('/',[UserController::class, "index"]);
